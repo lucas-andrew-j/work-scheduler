f881c7a14d2ff287f38c0815a55bceea4a544a97
Andrew <lucas.andrew.j@proton.me>
2025-11-14 23:55:15 -0800

    Updates structs for permutation-based scheduling

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use std::collections::HashMap;
+use std::collections::{BTreeMap, HashMap};
 
 type TaskId = usize;
+type Configuration = usize;
+type Resource = usize;
 
 fn main() {
     println!("Hello, world!");
@@ -8,7 +10,6 @@ fn main() {
 
 struct Schedule {
     tasks: HashMap<TaskId, Task>,
-    prioritized_tasks: Vec<TaskId>,
     ties: HashMap<TaskId, Vec<TaskId>>,
     resource_calendars: Option<HashMap<String, Resource_Calendar>>,
     config_calendar: Option<Configuration_Calendar>,
@@ -17,16 +18,16 @@ struct Schedule {
 struct Task {
     task_id: TaskId,
     task_name: String,
-    priority: usize,
     du: usize,
-    scheduled: bool,
     early_start: usize,
     early_finish: usize,
     late_start: usize,
     late_finish: usize,
     manual_start: usize,
-    resource_needs: Option<HashMap<String, Resource_Calendar>>,
-    config_needs: Option<Configuration_Calendar>,
+    resource_needs: Option<BTreeMap<Resource, usize>>,
+    config_needs: Option<Vec<Configuration>>,
+    config_latch: Option<Vec<Configuration>>,
+    config_unlatch: Option<TaskId>,
 }
 
 struct Resource_Calendar {
