d4aca4082ea0e2d29b8095279dbc7f1f4b45f11a
Andrew <lucas.andrew.j@proton.me>
2025-11-25 22:17:37 -0800

    Updates to use standard schedule methods

--- src/main.rs
+++ src/main.rs
@@ -1,98 +1,102 @@
-use std::cmp::max;
-use std::collections::{BTreeMap, HashMap};
-use std::iter::zip;
+use std::collections::{BTreeMap, BTreeSet, HashMap };
 
 type TaskId = usize;
-type Configuration = usize;
 type Resource = usize;
+type Shift = u8;
+type Interval = usize;
 
 fn main() {
-    println!("Hello, world!");
+    let mut tasks: HashMap<TaskId, Task> = HashMap::new();
+    tasks.insert(1, Task {
+        task_id: 1,
+        task_name: "Task 1".to_owned(),
+        du: 1,
+        shifts: 1,
+        early_start: None,
+        early_finish: None,
+        late_start: None,
+        late_finish: None,
+        manual_start: None,
+        resource_needs: None,
+    });
+    tasks.insert(2, Task {
+        task_id: 2,
+        task_name: "Task 2".to_owned(),
+        du: 1,
+        shifts: 1,
+        early_start: None,
+        early_finish: None,
+        late_start: None,
+        late_finish: None,
+        manual_start: None,
+        resource_needs: None,
+    });
+
+    let schedule = Schedule::new(
+        tasks,
+        HashMap::new(),
+        1);
+
+    schedule.contains_cycle();
 }
 
+#[derive(Debug)]
 struct Schedule {
     tasks: HashMap<TaskId, Task>,
     ties: HashMap<TaskId, Vec<TaskId>>,
-    resource_calendars: Option<HashMap<String, Resource_Calendar>>,
-    config_calendar: Option<Configuration_Calendar>,
-    config_compatibility: Vec<Vec<bool>>,
+    resource_demand: BTreeMap<String, usize>,
+    shifts: Shift,
 }
 
+#[derive(Debug)]
 struct Task {
     task_id: TaskId,
     task_name: String,
     du: usize,
-    early_start: usize,
-    early_finish: usize,
-    late_start: usize,
-    late_finish: usize,
-    manual_start: usize,
-    resource_needs: Option<BTreeMap<Resource, Vec<isize>>>,
-    config_needs: Option<Vec<Configuration>>,
-    config_latch: Option<Vec<Configuration>>,
-    config_unlatch: Option<TaskId>,
+    shifts: Shift,
+    early_start: Option<(Shift, Interval)>,
+    early_finish: Option<(Shift, Interval)>,
+    late_start: Option<(Shift, Interval)>,
+    late_finish: Option<(Shift, Interval)>,
+    manual_start: Option<(Shift, Interval)>,
+    resource_needs: Option<Vec<(Resource, Vec<isize>)>>,
 }
 
-struct Resource_Calendar {
-    calendar: Vec<Vec<isize>>,
-}
-
-impl Resource_Calendar {
-    fn new() -> Resource_Calendar {
-        Resource_Calendar { calendar: Vec::new() }
+impl Schedule {
+    fn new(tasks: HashMap<TaskId, Task>, ties: HashMap<TaskId, Vec<TaskId>>, shifts: u8) -> Schedule {
+        Schedule {
+            tasks: tasks,
+            ties: ties,
+            resource_demand: BTreeMap::new(),
+            shifts: shifts,
+        }
     }
 
-    pub fn add_resource_availability(&mut self, resource_availability: Vec<isize>) -> Resource {
-        self.calendar.push(resource_availability);
+    fn contains_cycle(&self) -> bool {
+        let mut tasks_without_preds: Vec<TaskId> = Vec::new();
 
-        self.calendar.len() - 1
-    }
-
-    fn find_first_resource_calendar_compatibility (&self, resource_needs: BTreeMap<Resource, Vec<isize>>) -> Option<usize> {
-        let mut first_compatible_intervals: BTreeMap<Resource, usize> = BTreeMap::new();
-        let mut min_interval = 0;
-
-        loop {
-            for (resource, needs) in resource_needs.iter() {
-                first_compatible_intervals.insert(resource.clone(), find_first_compatible_resource_window(&self.calendar[*resource], needs, min_interval)?);
-            }
-
-            let mut first_result = true;
-            let mut all_results_same = true;
-
-            for (_, &result) in first_compatible_intervals.iter() {
-                if first_result {
-                    first_result = false;
-                    min_interval = result;
-                } else if min_interval != result {
-                    all_results_same = false;
-                    min_interval = max(min_interval, result);
-                }
-            }
-
-            if all_results_same {
-                return Some(min_interval);
+        for task in self.tasks.iter() {
+            if !self.ties.contains_key(&task.1.task_id) {
+                tasks_without_preds.push(task.1.task_id);
             }
         }
 
-        None
-    }
-}
+        let mut path: BTreeSet<TaskId> = BTreeSet::new();
 
-fn find_first_compatible_resource_window (resource_calendar: &Vec<isize>, resource_need: &Vec<isize>, starting_interval: usize) -> Option<usize> {
-    'outer: for i in 0..resource_calendar.len() - resource_need.len() {
-        for j in 0..resource_need.len() {
-            if resource_calendar[i + j] < resource_need[j] {
-                continue 'outer;
-            }
+        for task_id in tasks_without_preds {
+            //check for a cycle from here
         }
 
-        return Some(i);
+        false
     }
-
-    None
 }
 
-struct Configuration_Calendar {
-    
-}
+// impl Task {
+//     fn new()
+// }
+
+
+
+
+
+
